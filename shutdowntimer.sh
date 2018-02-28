b9f2e26f34e575034321f3382662dde5e953a295
L4ma <m.damm@mnet-online.de>
2018-02-28 08:10:37 +0100

    file changed

--- shutdowntimer.sh
+++ shutdowntimer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "When should the system be down? " time
+read -p "In how many minutes should the system shut down? " time
 sudo shutdown $time
 
 echo "The system will shut down in $time minute/s."
